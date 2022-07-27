91b36233038b4c2bd3fdb5c1d73821eeb02d82d3
matheusoliv23 <matheushenri1996@gmail.com>
2022-07-27 18:04:36 -0300

    feat: Initial login to room

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import "./App.css";
 import io from "socket.io-client";
+import { useState } from "react";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {};
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <h3>Entrar no chat</h3>
+      <input
+        type="text"
+        placeholder="Seu nome"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Nome da sala"
+        onChange={(e) => setRoom(e.target.value)}
+      />
+      <button>Entrar no sala</button>
     </div>
   );
 }
