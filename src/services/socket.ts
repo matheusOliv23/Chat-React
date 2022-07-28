352570a358bbbd7ee306bfa0e3fadb0dd5a452ed
matheusoliv23 <matheushenri1996@gmail.com>
2022-07-28 16:54:35 -0300

    fix: changing url again to prod

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,4 +1,3 @@
 import io from "socket.io-client";
 
-//export const socket = io.connect("https://live-chat-messages.herokuapp.com/");
-export const socket = io.connect("http://localhost:5000/");
+export const socket = io.connect("https://live-chat-messages.herokuapp.com/");
