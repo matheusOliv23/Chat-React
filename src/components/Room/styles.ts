be489f79f3bc6eddb7da9b0abeff23a6f30ed448
matheusoliv23 <matheushenri1996@gmail.com>
2022-07-28 05:09:50 -0300

    style: room login

--- src/components/Room/styles.ts
+++ src/components/Room/styles.ts
@@ -3,11 +3,26 @@ import styled from "styled-components";
 export const JoinChatContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   text-align: center;
 
   h3 {
     font-size: 2.5rem;
     margin-bottom: 1rem;
+  }
+
+  button {
+    width: 225px;
+    height: 50px;
+    margin: 7px;
+    border: none;
+    border-radius: 5px;
+    padding: 5px;
+    font-size: 16px;
+    background: #43a047;
+    color: #fff;
+    cursor: pointer;
   }
 `;
 
